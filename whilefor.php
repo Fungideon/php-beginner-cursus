e145ee50441e3a8489379537256e910777149e74
Timo Moederzoon <tigimoti@gmail.com>
2016-02-19 12:28:08 +0100

    opdracht kerstboom en while naar for loop

---
+++ whilefor.php
@@ -0,0 +1,13 @@
+<?php
+    $x = 35;
+    while($x >= 25) {
+        echo "hoppelepee";
+        $x--;
+    }
+
+    echo "<br>";
+
+    for($i = 35; $i >= 25; $i--){
+        echo "hoppelepee";
+    }
+?>
