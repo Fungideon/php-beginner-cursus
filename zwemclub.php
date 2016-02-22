d5b03ccfe7e0068e2a70415f5b9725b868189175
Timo Moederzoon <tigimoti@gmail.com>
2016-02-22 10:52:59 +0100

    zwemclub fix

--- zwemclub.php
+++ zwemclub.php
@@ -9,7 +9,7 @@
         echo "<td>".$label."</td><td>".$waarde."</td><td>";
         for($i = 0; $i < $waarde; $i++) {
             if($i%5==4){
-                echo "<img src='zwemmen-oefening_318-27630.png' style='height=\"50px\"' >";
+                echo "<img src='zwemmen-oefening_318-27630.png' style=\"height:50px;width:50px;\">";
             }
         }
         echo "</td>";
