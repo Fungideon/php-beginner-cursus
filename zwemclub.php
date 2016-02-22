53048c60484876ea5c159a89bc91de6124336253
Timo Moederzoon <tigimoti@gmail.com>
2016-02-22 10:34:38 +0100

    opdracht kapper en zwemclub

---
+++ zwemclub.php
@@ -0,0 +1,20 @@
+<table>
+    <?php
+        $zwemclubs["De spartelkuikens"] = 25;
+        $zwemclubs["De waterbuffels"] = 32;
+        $zwemclubs["Plonsmderin"] = 11;
+        $zwemclubs["Bommetje"] = 23;
+    foreach ($zwemclubs as $label => $waarde){
+        echo "<tr>";
+        echo "<td>".$label."</td><td>".$waarde."</td><td>";
+        for($i = 0; $i < $waarde; $i++) {
+            if($i%5==4){
+                echo "<img src='zwemmen-oefening_318-27630.png' style='height=\"50px\"' >";
+            }
+        }
+        echo "</td>";
+        echo "</tr>";
+    }
+    ?>
+
+</table>
